f0cd947e7ea1b660fc7ff2d9d596eb47668f8065
RichardAdrian123 <racruz-es@udabol.edu.bo>
2024-05-22 09:49:33 -0400

    Almacenamiento de objetos part 6.1, Crear una base de datos de artículos utilizando Scriptable Objects, tambien se cambio el ID que identificaba al tipo de basura a IDTipo y se agrego un elementoID para identificar al objeto

--- Assets/Scripts/InventarioScripts/InvetarioRanura.cs
+++ Assets/Scripts/InventarioScripts/InvetarioRanura.cs
@@ -6,65 +6,86 @@ using UnityEditor;
 using UnityEngine;
 
 [System.Serializable]
-public class InvetarioRanura
+public class InvetarioRanura : ISerializationCallbackReceiver
 {
   [SerializeField] private int idTipo;
   [SerializeField] private DatosInventario datosElemento;
+  [SerializeField] private int _elementoID = -1;
   [SerializeField] private int capacidadPila;
 
   public int IDTipo => idTipo;
   public DatosInventario DatosElemento => datosElemento;
   public int CapacidadPila => capacidadPila;
 
-  public InvetarioRanura(DatosInventario fuente, int cantidad, int IdTipo){
+  public InvetarioRanura(DatosInventario fuente, int cantidad, int IdTipo)
+  {
     this.idTipo = IdTipo;
     datosElemento = fuente;
+    _elementoID = datosElemento.ID;
     capacidadPila = cantidad;
   }
-  public InvetarioRanura(){
+  public InvetarioRanura()
+  {
     LimpiarRanura();
   }
-  public void LimpiarRanura(){
+  public void LimpiarRanura()
+  {
     idTipo = -1;
     datosElemento = null;
+    _elementoID = -1;
     capacidadPila = -1;
   }
-  public void AsignarElemento(InvetarioRanura invRanura){
-    if(datosElemento == invRanura.datosElemento){
+  public void AsignarElemento(InvetarioRanura invRanura)
+  {
+    if (datosElemento == invRanura.datosElemento)
+    {
       AgregarPila(invRanura.capacidadPila);
-    } else {
+    }
+    else
+    {
       idTipo = invRanura.idTipo;
       datosElemento = invRanura.datosElemento;
+      _elementoID = datosElemento.ID;
       capacidadPila = 0;
       AgregarPila(invRanura.capacidadPila);
     }
   }
-  public void ActualizarInventarioRanura(DatosInventario datos, int cantidad){
+  public void ActualizarInventarioRanura(DatosInventario datos, int cantidad)
+  {
     idTipo = datos.IDTipo;
     datosElemento = datos;
+    _elementoID = datosElemento.ID;
     capacidadPila = cantidad;
     Debug.Log("ID" + idTipo);
     Debug.Log("elementos: " + datosElemento);
     Debug.Log("capacidad: " + capacidadPila);
   }
-  public bool EspacioRestantePila(int agregarCantidad, out int cantidadRestante){
+  public bool EspacioRestantePila(int agregarCantidad, out int cantidadRestante)
+  {
     cantidadRestante = DatosElemento.maxCapacidadPila - capacidadPila;
     return EspacioRestantePila(agregarCantidad);
   }
-  public bool EspacioRestantePila(int agregarCantidad){
-    if(capacidadPila + agregarCantidad <= datosElemento.maxCapacidadPila){
+  public bool EspacioRestantePila(int agregarCantidad)
+  {
+    if (capacidadPila + agregarCantidad <= datosElemento.maxCapacidadPila)
+    {
       return true;
-    } else { return false; }
+    }
+    else { return false; }
   }
-  public void AgregarPila(int cantidad){
+  public void AgregarPila(int cantidad)
+  {
     capacidadPila += cantidad;
   }
-  public void EliminarDePila(int cantidad){
+  public void EliminarDePila(int cantidad)
+  {
     capacidadPila -= cantidad;
   }
 
-  public bool PilaDividida(out InvetarioRanura pilaDividida){
-    if(capacidadPila <= 1){
+  public bool PilaDividida(out InvetarioRanura pilaDividida)
+  {
+    if (capacidadPila <= 1)
+    {
       pilaDividida = null;
       return false;
     }
@@ -73,5 +94,17 @@ public class InvetarioRanura
 
     pilaDividida = new InvetarioRanura(datosElemento, mediaPila, idTipo);
     return true;
+  }
+
+  public void OnBeforeSerialize()
+  {
+    
+  }
+
+  public void OnAfterDeserialize()
+  {
+    if(_elementoID == -1) return;
+    var bd = Resources.Load<BaseDatos>("BaseDeDatos");
+    datosElemento = bd.ObtenerElemento(_elementoID);
   }
 }
