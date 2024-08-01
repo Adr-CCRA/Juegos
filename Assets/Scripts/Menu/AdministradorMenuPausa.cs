b96f0041ea0638bb97c8e98d91cf7682c6c5261c
RichardAdrian123 <racruz-es@udabol.edu.bo>
2024-07-31 22:31:30 -0400

    UI Botones 2 - UI MenuPausa2, se agrego imagen para el boton de abrir saco de reciclaje (Mochila), se agregaron fondos de imagen y botones para el MenuPausa

--- Assets/Scripts/Menu/AdministradorMenuPausa.cs
+++ Assets/Scripts/Menu/AdministradorMenuPausa.cs
@@ -10,29 +10,35 @@ public class AdministradorMenuPausa : MonoBehaviour
   public GameObject ObjetoMenuPausa;
   public bool pausa = false;
   public GameObject MenuSalir;
+  public GameObject MenuPartida;
   public Button botonPausa;
+  public List<GameObject> imagenesDeFondo;
+  private int indiceImagenActual = 0;
   void Start()
   {
     ObjetoMenuPausa.SetActive(false);
     MenuSalir.SetActive(false);
+    MenuPartida.SetActive(false);
     botonPausa.onClick.AddListener(Pausa);
+    DesactivarTodasLasImagenes();
   }
   void Update()
   {
-    if(Keyboard.current.pKey.wasPressedThisFrame)
+    if (Keyboard.current.pKey.wasPressedThisFrame)
     {
       Pausa();
     }
   }
-  public void Pausa(){
-    
-      if(pausa == false)
-      {
-        ObjetoMenuPausa.SetActive(true);
-        pausa = true;
+  public void Pausa()
+  {
+    if (pausa == false)
+    {
+      ObjetoMenuPausa.SetActive(true);
+      pausa = true;
 
-        Time.timeScale = 0;
-      }
+      Time.timeScale = 0;
+      CambiarImagenDeFondo();
+    }
   }
   public void Reanudar()
   {
@@ -40,6 +46,8 @@ public class AdministradorMenuPausa : MonoBehaviour
     pausa = false;
     Time.timeScale = 1;
     MenuSalir.SetActive(false);
+    MenuPartida.SetActive(false);
+    DesactivarTodasLasImagenes();
   }
 
   public void IrMenu(string MenuPrincipal)
@@ -52,5 +60,22 @@ public class AdministradorMenuPausa : MonoBehaviour
   {
     Application.Quit();
     Debug.Log("Salio del Juego");
+  }
+  private void CambiarImagenDeFondo()
+  {
+    DesactivarTodasLasImagenes(); // Desactiva la imagen actual
+    if (imagenesDeFondo.Count > 0)
+    {
+      imagenesDeFondo[indiceImagenActual].SetActive(true); // Activa la siguiente imagen
+      indiceImagenActual = (indiceImagenActual + 1) % imagenesDeFondo.Count; // Pasar a la siguiente imagen
+    }
+  }
+
+  private void DesactivarTodasLasImagenes()
+  {
+    foreach (var imagen in imagenesDeFondo)
+    {
+      imagen.SetActive(false);
+    }
   }
 }
